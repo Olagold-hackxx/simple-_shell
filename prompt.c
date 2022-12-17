c093c5ef6208249b6d252e6a0cec039497d348a0
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2022-12-17 04:45:29 -0800

    Prompt created

---
+++ prompt.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * print_prompt - print command prompt
+ * @status: determin which prompt to print
+ * Return: void
+ */
+
+void print_prompt(int status)
+{
+	while (isatty(STDIN_FILENO))
+	{
+		if (status == 1)
+			write(STDOUT_FILENO, "$", 2);
+		else if (status == 2)
+			write(STDOUT_FILENO, "\n$", 3);
+		else
+			write(STDOUT_FILENO, "\n", 2);
+	}
+
+}
