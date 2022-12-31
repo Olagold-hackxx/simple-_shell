20e4d6c77e7d112cba02c8c640241310a97680a1
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2022-12-31 10:06:13 +0000

    Command Line args executes

--- getline.c
+++ getline.c
@@ -9,9 +9,9 @@ size_t _getline(char *argv[])
 {
 	char *buf = NULL;
 	size_t buf_size = 0;
-	size_t read_size;
-	char *copied;
-	int i;
+	ssize_t read_size, i;
+	char *copied, *arg[20], *delim = " ";
+	int token;
 
 	read_size = getline(&buf, &buf_size, stdin);
 	if (read_size < 0)
@@ -21,11 +21,21 @@ size_t _getline(char *argv[])
 	if (copied == NULL)
 		exit(EXIT_FAILURE);
 
+	/* a loop to copy input from getline */
 	for(i = 0; i < read_size-1; i++)
 	{
 		copied[i] = buf[i];
 	}
-	char *arg[] = {copied, NULL};
-	exec_exe(copied, arg, NULL);
+	arg[0] = strtok(copied, delim);
+	token = 0;
+	while (arg[token])
+	{
+		token++;
+		arg[token] = strtok(NULL, delim);
+	}
+	arg[token+1] = NULL;
+
+	exec_exe(arg[0], arg, NULL);
+
 	return (read_size);
 }
--- prompt.c
+++ prompt.c
@@ -2,7 +2,7 @@
 
 /**
  * print_prompt - print command prompt
- * @status: determin which prompt to print
+ * @status: determine which prompt to print
  * Return: void
  */
 
@@ -12,13 +12,11 @@ int print_prompt(int status)
 	{
 		if (status == 1)
 		{
-			write(STDOUT_FILENO, "$", 2);
+			write(STDOUT_FILENO, "$ ", 2);
 			return (0);
 		}
 		else if (status == 2)
-			write(STDOUT_FILENO, "\n$", 3);
-		else
-			write(STDOUT_FILENO, "\n", 2);
+			write(STDOUT_FILENO, "\n$ ", 3);
 	}
-
+	return (0);
 }
