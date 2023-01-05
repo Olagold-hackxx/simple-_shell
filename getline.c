37841ffb286f33cc77de3bbfe06b0566097670e1
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-01-05 10:07:16 +0000

    Free'd memory allocs

--- getline.c
+++ getline.c
@@ -8,26 +8,36 @@
 
 size_t _getline(char **argv)
 {
-	char *buf = NULL;
-	size_t buf_size = 0;
+	char *buf;
+	size_t buf_size = 100;
 	ssize_t read_size, i;
 	char *copied, *arg[100], *delim = " ";
 	int token;
+
+	buf = malloc(sizeof(char) * buf_size);
+	if (buf == NULL)
+	{
+		free(buf);
+		exit(EXIT_FAILURE);
+	}
 
     /*read user input from stdin */
 	read_size = getline(&buf, &buf_size, stdin);
 	if (read_size < 0)
 		perror(argv[0]);
-
+	
 	copied = malloc(sizeof(char) * read_size);
 	if (copied == NULL)
+	{
+		free(copied);
 		exit(EXIT_FAILURE);
-
+	}
 	/* a loop to copy input from getline */
 	for (i = 0; i < read_size - 1; i++)
 	{
 		copied[i] = buf[i];
 	}
+	
 	/*tokenize string*/
 	arg[0] = strtok(copied, delim);
 	token = 0;
@@ -36,11 +46,12 @@ size_t _getline(char **argv)
 		token++;
 		arg[token] = strtok(NULL, delim);
 	}
-	/*append NULL to arg incase of no args to cmd*/
-	arg[token + 1] = NULL;
+
+	/*append NULL to arg incase no args passed to cmd*/
 
 	exec_exe(arg[0], arg, NULL);
-
+	free(buf);
+	free(copied);
 	return (read_size);
 }
 
@@ -57,16 +68,24 @@ size_t readline(char **argv)
 	char *buf, *arg[50], *copied, *delim = " ";
 	ssize_t read_size;
 
+	/*alloc memory for buf*/
 	buf = malloc(sizeof(char) * count);
-	if (!buf)
+	if (buf == NULL)
+	{
+		free(buf);
 		exit(EXIT_FAILURE);
+	}
 	read_size = read(STDIN_FILENO, buf, count);
 	if (read_size < 0)
 		perror(argv[0]);
+	/*allocate memory for copied*/
+	/* a loop to copy input from read */
 	copied = malloc(sizeof(char) * read_size);
 	if (copied == NULL)
+	{
+		free(copied);
 		exit(EXIT_FAILURE);
-	/* a loop to copy input from read */
+	}
 	for (i = 0; i < read_size - 1; i++)
 	{
 		copied[i] = buf[i];
@@ -78,8 +97,11 @@ size_t readline(char **argv)
 		token++;
 		arg[token] = strtok(NULL, delim);
 	}
-	arg[token + 1] = NULL;
-
+	
+	arg[token] = NULL;
+	
 	exec_exe(arg[0], arg, NULL);
+	free(buf);
+	free(copied);
 	return (read_size);
 }
--- shell.h
+++ shell.h
@@ -15,7 +15,7 @@ size_t _getline(char **argv);
 size_t readline(char **argv);
 int exec_exe(char *path, char **arg, char **envp);
 ssize_t read(int fd, void *buf, size_t count);
-
+void free_mem(char *mem_ptr, int mem_len);
 
 
 
